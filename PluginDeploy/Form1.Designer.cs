e86e4000707d27e09520b54cf4c9b1e59e9278dd
Benson Che <bche@resdat.com>
2015-06-24 10:31:19 -0700

    init

---
+++ PluginDeploy/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PluginDeploy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(13, 13);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(259, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "c:\\code\\intranet";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(196, 40);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ PluginDeploy/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PluginDeploy
+{
+    public partial class Form1 : Form
+    {
+        private const string outputDir = @"c:\temp\plugins";
+        private const string prefix = @"RDI.Service.Plugins";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            btnGo.Enabled = false;
+
+            DirectoryInfo output = new DirectoryInfo(outputDir);
+            if(output.Exists)
+            {
+                foreach (var f in output.GetFiles())
+                    f.Delete();
+                output.Delete();
+            }
+            output.Create();
+
+            DirectoryInfo dir = new DirectoryInfo(txtInput.Text);
+
+            foreach(var d in dir.GetDirectories())
+            {
+                if(d.Name.StartsWith(prefix))
+                {
+                    processDirectory(d);
+                }
+            }
+
+            btnGo.Enabled = true;
+        }
+
+        private void processDirectory(DirectoryInfo dir)
+        {
+            DirectoryInfo bin = new DirectoryInfo(Path.Combine(dir.FullName, @"bin\Release"));
+            if (bin.Exists)
+            {
+                foreach (var f in bin.GetFiles())
+                {
+                    File.Copy(f.FullName, Path.Combine(outputDir, f.Name), true);
+                }
+            }
+        }
+    }
+}
